d46cea1ed577f34da527398d8f9b41d9a95449c7
weihao <turtleking999@gmail.com>
2017-06-05 23:46:18 +0800

    cust/createAccount.js

--- code/functions_ez/functions/index.js
+++ code/functions_ez/functions/index.js
@@ -1,8 +1,15 @@
 const functions = require('firebase-functions');
+const cust = require('cust');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+
+exports.createAccount = cust.https.onRequest((request, response) => {
+    sendCreateAccount();
+});
+
